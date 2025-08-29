d4593ddf8ec398d74b5411a57e2078263a3c0592
DJoztin <al267187@edu.uaa.mx>
2025-08-29 16:18:03 -0600

    practica2

--- Practica2/app/src/main/java/com/example/practica2/MainActivity.kt
+++ Practica2/app/src/main/java/com/example/practica2/MainActivity.kt
@@ -1,39 +1,118 @@
 package com.example.practica2
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.practica2.ui.theme.Practica2Theme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rgNivel: RadioGroup
+    private lateinit var rbBasico: RadioButton
+    private lateinit var rbIntermedio: RadioButton
+    private lateinit var rbAvanzado: RadioButton
+
+
+    private lateinit var cbKotlin: CheckBox
+    private lateinit var cbJetpack: CheckBox
+    private lateinit var cbFirebase: CheckBox
+    private lateinit var btnVerSeleccion: Button
+    private lateinit var tvResultado: TextView
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
+        rgNivel = findViewById(R.id.rgNivel)
+        rbBasico = findViewById(R.id.radioButton)
+        rbIntermedio = findViewById(R.id.radioButton2)
+        rbAvanzado = findViewById(R.id.radioButton3)
+        cbKotlin = findViewById(R.id.cbKotlin)
+        cbJetpack = findViewById(R.id.cbJetpack)
+        cbFirebase = findViewById(R.id.cbFirebase)
+        btnVerSeleccion = findViewById(R.id.btnVerSeleccion)
+        tvResultado = findViewById(R.id.tvResultado)
+
+
+        //listener Radiogroup
+
+        rgNivel.setOnCheckedChangeListener {
+            _,checkedId ->
+            val nivel = when (checkedId){
+                R.id.radioButton -> "Basico"
+                R.id.radioButton2 -> "Intermedio"
+                R.id.radioButton3 -> "Avanzado"
+                else-> "Ninguno"
+
+            }
+            Log.i("Nivel","Nicel cambio a $nivel" )
+        }
+
+        //listener para checkbox
+
+        val checkListener = CompoundButton.OnCheckedChangeListener{
+            button, isChecked ->
+            Log.i("Checkbox", "Checkbox ${button.text}: $isChecked")
+        }
+
+        cbKotlin.setOnCheckedChangeListener(checkListener)
+        cbJetpack.setOnCheckedChangeListener(checkListener)
+        cbFirebase.setOnCheckedChangeListener(checkListener)
+
+        //Accion del boton
+
+        btnVerSeleccion.setOnClickListener {
+            val nivel = when (rgNivel.checkedRadioButtonId){
+                R.id.radioButton -> "Basico"
+                R.id.radioButton2 -> "Intermedio"
+                R.id.radioButton3 -> "Avanzado"
+                else-> "Ninguno"
+            }
+
+            //buildList + ifEmpty
+            val tecnologias = buildList {
+                if(cbKotlin.isChecked) add("Kotlin")
+                if(cbJetpack.isChecked) add("Jetpack")
+                if(cbFirebase.isChecked) add("Firebase")
+            }.joinToString(",").ifEmpty { "Ninguna" }
+
+            val texto = "Nivel: $nivel\nTecnologias: $tecnologias"
+            tvResultado.text = texto
+            Toast.makeText(this, "Leido", Toast.LENGTH_LONG).show()
+            
+            //Restauración
+            savedInstanceState?.let{
+                state ->
+                rgNivel.check(state.getInt("nivelCheckedId", -1))
+                cbKotlin.isChecked = state.getBoolean("cbKotlin", false)
+                cbJetpack.isChecked = state.getBoolean("cbJetpack", false)
+                cbFirebase.isChecked = state.getBoolean("cbFirebase", false)
+                tvResultado.text = state.getString("resultado", "Resultado aparece aqui:")
+            }
+
+        }
+
+
+    }
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putInt("nivelCheckedId", rgNivel.checkedRadioButtonId)
+        outState.getBoolean("cbKotlin", cbKotlin.isChecked)
+        outState.getBoolean("cbJetpack", cbJetpack.isChecked)
+        outState.getBoolean("cbFirebase", cbFirebase.isChecked)
+        outState.getString("resultado", tvResultado.text.toString())
     }
 
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Practica2Theme {
-        Greeting("Android")
-    }
-}
